ae34afaa81d2ac5e1e49cea519e1c4ff9d401018
Yash-Yelave <rostopedia@gmail.com>
2026-05-24 10:46:06 +0530

    feat: initialize main application component with core layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
 import FeatureGrid from './components/FeatureGrid';
 import FAQ from './components/FAQ';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <FeatureGrid />
-      <FAQ />
+    <div className="min-h-screen bg-white font-sans text-slate-900 flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <FeatureGrid />
+        <FAQ />
+      </main>
+      <Footer />
     </div>
   );
 }
